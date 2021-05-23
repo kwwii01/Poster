46d0c6451f377b0f4607056754b91fbd30af8c78
kwwiiGod <mrstream1@gmail.com>
2021-05-23 22:59:37 +0300

    Bootstrap base front-end added

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,9 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: "User"
   has_many :comments, dependent: :destroy
-  has_one_attached :picture
+  has_one_attached :picture, dependent: :destroy
 
-  VALID_CATEGORIES = ['Scientific', 'Politics', 'TV Series', 'Anime', 'Films', 'Technologies']
+  VALID_CATEGORIES = ['Scientific', 'Politics', 'TV Series', 'Anime', 'Films', 'Technologies', 'Music']
 
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
